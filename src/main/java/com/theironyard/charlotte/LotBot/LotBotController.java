3435639c77bce0bceb441ddb4f63d3925a319d00
Christopher Ingram <cingram82@gmail.com>
2017-09-29 16:24:02 -0400

    stoped extra lots error

--- src/main/java/com/theironyard/charlotte/LotBot/LotBotController.java
+++ src/main/java/com/theironyard/charlotte/LotBot/LotBotController.java
@@ -2,57 +2,62 @@ package com.theironyard.charlotte.LotBot;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class LotBotController {
   List<Lot> parkingLots = new ArrayList<>();
+  List<Transaction> transactionLog = new ArrayList<>();
 
   public void postConstruct() {
-    parkingLots.add(Lot.createLot(10,10));
-    parkingLots.add(Lot.createLot(20,25));
-    parkingLots.add(Lot.createLot(30,30));
-    parkingLots.add(Lot.createLot(40,10));
-    parkingLots.add(Lot.createLot(50,4));
+    if (parkingLots.size() < 1) {
+      parkingLots.add(Lot.createLot(10, 10));
+      parkingLots.add(Lot.createLot(20, 25));
+      parkingLots.add(Lot.createLot(30, 30));
+      parkingLots.add(Lot.createLot(40, 10));
+      parkingLots.add(Lot.createLot(50, 4));
+    }
   }
 
-  //  GET   /lots         Get a list of all lots in the system, including # of spots.
   @CrossOrigin
   @RequestMapping(path = "/lots", method = RequestMethod.GET)
   private List<Lot> getAllLots(){
     postConstruct();
-
     return parkingLots;
   }
 
 
-  //  GET   /lots/<id>    Get a list of the status of all spots in the specified lot,
-  //                      including the license plate # of anyone parked.
   @CrossOrigin
   @RequestMapping(path = "/lots/{id}", method = RequestMethod.GET)
-  private Space[] getSpaces(@PathVariable("id") int id){
-    return parkingLots.get(id).getSpaces();
+  private Lot getSpaces(@PathVariable("id") int id){
+    return parkingLots.get(id);
   }
 
-  //  POST  /lots/<id>    Park a new car in the specified lot. Must send the Vehicle object
+  //  POST  /lots/<id>/<index>    Park a new car in the specified lot. Must send the Vehicle object
   //                      in the request body.
 
   @CrossOrigin
   @RequestMapping(path = "/lots/{id}/{index]",method = RequestMethod.POST)
   private void addCar(@RequestBody Car car, @PathVariable("index") int index, @PathVariable("id") int id){
 
-    Space[] listSpaces= parkingLots.get(index).getSpaces();
-    listSpaces[id].setVehicle(car);
+    Space[] listSpaces= parkingLots.get(id).getSpaces();
+    listSpaces[index].setVehicle(car);
+    transactionLog.add(Transaction.createTransaction(car, LocalDateTime.now(), parkingLots.get(id).getPrice()));
   }
+
+//  PUT   /lots/<id>/<spot>  Open up the specified spot and return the total owed.
+  @CrossOrigin
+  @RequestMapping(path = "/lots/{id}/{index]",method = RequestMethod.PUT)
+  private void removeCar(@RequestBody Car car, @PathVariable("index") int index, @PathVariable("id") int id){
+
+   Space[] listSpaces= parkingLots.get(index).getSpaces();
+    listSpaces[id].setVehicle(car);
+}
 }
 
-//  @CrossOrigin
-//  @RequestMapping(path = "/books", method = RequestMethod.POST)
-//  public void addBook(@RequestBody Book book) {
-//    books.add(book);
-//
-//
+
 
 //  The API
 //  There are two key concepts that the entire Lot Bot system is based on: lots and cars. The API should support the following operations, and all should use JSON for encoding request and response bodies.
@@ -60,12 +65,6 @@ public class LotBotController {
 //
 
 //Method  URL           Description
-
-
-
-
-
-//  PUT   /lots/<id>/<spot>  Open up the specified spot and return the total owed.
 
 //  GET   /transactions      Return a list of all transactions, along with the bill and
 //                           license plate number of the charged vehicle.
--- src/main/java/com/theironyard/charlotte/LotBot/Transaction.java
+++ src/main/java/com/theironyard/charlotte/LotBot/Transaction.java
@@ -1,11 +1,13 @@
 package com.theironyard.charlotte.LotBot;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Transaction {
+  private int id;
+  private static int ID_COUNTER;
   private Vehicle Vehicle;
-  private Date checkedInDate;
-  private Date checkedOutDate;
+  private LocalDateTime checkedInDate;
+  private LocalDateTime checkedOutDate;
   private double price;
 
   public Vehicle getVehicle() {
@@ -16,19 +18,19 @@ public class Transaction {
     this.Vehicle = vehicle;
   }
 
-  public Date getCheckedInDate() {
+  public LocalDateTime getCheckedInDate() {
     return checkedInDate;
   }
 
-  public void setCheckedInDate(Date checkedInDate) {
+  public void setCheckedInDate(LocalDateTime checkedInDate) {
     this.checkedInDate = checkedInDate;
   }
 
-  public Date getCheckedOutDate() {
+  public LocalDateTime getCheckedOutDate() {
     return checkedOutDate;
   }
 
-  public void setCheckedOutDate(Date checkedOutDate) {
+  public void setCheckedOutDate(LocalDateTime checkedOutDate) {
     this.checkedOutDate = checkedOutDate;
   }
 
@@ -44,11 +46,21 @@ public class Transaction {
 
   }
 
-  public Transaction(Vehicle Vehicle, Date checkedInDate, Date checkedOutDate, double price) {
+  public int getId() {
+    return id;
+  }
 
-    this.Vehicle = Vehicle;
-    this.checkedInDate = checkedInDate;
-    this.checkedOutDate = checkedOutDate;
-    this.price = price;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public static Transaction createTransaction(Vehicle Vehicle, LocalDateTime checkedInDate, double price) {
+    Transaction t = new Transaction();
+    t.setVehicle(Vehicle);
+    t.setCheckedInDate(checkedInDate);
+    t.setPrice(price);
+    t.setId(ID_COUNTER);
+    ID_COUNTER++;
+    return t;
   }
 }
